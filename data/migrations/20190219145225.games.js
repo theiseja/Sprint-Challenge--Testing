bd1a6a7aa89dc3d1459fbf2bbc25a93006ed1b72
Jesse Theisen <theiseja@gmail.com>
2019-04-20 11:57:12 -0500

    MVP: Added dependencies and setup knex

---
+++ data/migrations/20190219145225.games.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('games', function (table) {
+      table.increments();
+      table.string('title').notNullable().unique();
+      table.string('genre').notNullable();
+      table.string('releaseYear');
+   })
+};
+
+exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('games');
+};
