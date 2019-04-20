402a1a6f999e4ee6f63a8cc7d8bf2c2308b4cb48
Jesse Theisen <theiseja@gmail.com>
2019-04-20 12:32:54 -0500

    MVP: Wrote POST endpoint

--- api/server.js
+++ api/server.js
@@ -7,22 +7,32 @@ const server = express();
 server.use(express.json());
 
 server.get('/', async (req, res) => {
-    res.status(200).json({ api: 'up' });
+	res.status(200).json({ api: 'up' });
 });
 
 // Get all games
 
 server.get('/games', async (req, res) => {
-    const rows = await games.getAll();
+	const rows = await games.getAll();
 
-    res.status(200).json(rows);
-})
+	res.status(200).json(rows);
+});
 
 // Add a new game
 
 server.post('/games', (req, res) => {
-    const { title, genre, releaseYear} = req.body;
-    if(!title || !genre || !releaseYear){
-        
-    }
-})
+	const { title, genre, releaseYear } = req.body;
+	if (!title || !genre || !releaseYear) {
+		return res
+			.status(422)
+			.json({ errorMessage: 'Add Title, Genre or Release Year' });
+	}
+	games
+		.insert({ title, genre, releaseYear })
+		.then(game => {
+			res.status(201).json({ game });
+		})
+		.catch(error => {
+			res.status(422).json(error);
+		});
+});
